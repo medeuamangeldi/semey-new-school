8c6fe07adb8ebc9a7d9c7dcdb59542bbc335961a
Medeu Amangeldi <medcod49@gmail.com>
2024-02-15 13:48:12 -0600

    initial commit, navbar with animated burger menu

---
+++ src/app/_components/navMobile/NavMobile.tsx
@@ -0,0 +1,64 @@
+import { useClickAway } from "react-use";
+import { useRef } from "react";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { Squash as Hamburger } from "hamburger-react";
+import { routes } from "@/app/helpers/routes";
+import styles from "./nav-mobile.module.scss";
+
+export const NavMobile = (className: any) => {
+  const [isOpen, setOpen] = useState(false);
+  const ref = useRef(null);
+
+  useClickAway(ref, () => setOpen(false));
+
+  return (
+    <div ref={ref}>
+      <div className={styles['menu']}>
+        <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+      </div>
+
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+            className={styles["main"]}
+          >
+            <ul className={styles["grid"]}>
+              {routes.map((route, idx) => {
+                const { Icon } = route;
+
+                return (
+                  <motion.li
+                    initial={{ scale: 0, opacity: 0 }}
+                    animate={{ scale: 1, opacity: 1 }}
+                    transition={{
+                      type: "spring",
+                      stiffness: 260,
+                      damping: 20,
+                      delay: 0.1 + idx / 10,
+                    }}
+                    key={route.title}
+                    className={styles["list"]}
+                  >
+                    <a
+                      onClick={() => setOpen((prev) => !prev)}
+                      className={styles["item"]}
+                      href={route.href}
+                    >
+                      <span className="flex gap-1 text-lg">{route.title}</span>
+                      <Icon className="text-xl" />
+                    </a>
+                  </motion.li>
+                );
+              })}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
