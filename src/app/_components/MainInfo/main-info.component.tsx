18768e1d19f4245e0f67af4b00f0639d328be8c2
Medeu Amangeldi <medcod49@gmail.com>
2024-04-24 12:44:01 -0500

    fix/video

--- src/app/_components/MainInfo/main-info.component.tsx
+++ src/app/_components/MainInfo/main-info.component.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useRef } from "react";
 import styles from "./main-info.module.scss";
 import { useInView } from "framer-motion";
 import Image from "next/image";
+import clsx from "clsx";
 const MainInfo = () => {
   const myRef: any = useRef();
   const isInView = useInView(myRef, { once: false });
@@ -20,7 +22,7 @@ const MainInfo = () => {
       </span>
 
       <div className={styles["info-section-cards"]}>
-        <div className={styles["info-section-cards-card"]}>
+        <div className={clsx(styles["info-section-cards-card"])}>
           <div className={styles["info-section-cards-card-text"]}>
             <span className={styles["info-section-cards-card-text-title"]}>
               Elevate your{" "}
@@ -42,6 +44,98 @@ const MainInfo = () => {
             height={660}
             alt="main_image_first"
             className={styles["info-section-cards-card-image"]}
+          />
+        </div>
+        <div
+          className={clsx(
+            styles["info-section-cards-card"],
+            styles["info-section-cards-card-reverse"]
+          )}
+        >
+          <div className={styles["info-section-cards-card-text"]}>
+            <span className={styles["info-section-cards-card-text-title"]}>
+              Leading{" "}
+              <span
+                className={styles["info-section-cards-card-text-title-special"]}
+              >
+                Faculty
+              </span>
+            </span>
+            <div className={styles["info-section-cards-card-text-body"]}>
+              SNS's diverse faculty are leaders in their field - and committed
+              to the school’s mission to make a positive difference in the
+              world. Devoted to their craft they guide students to think beyond
+              the simple answers – preparing them for college and the workplace
+              beyond.
+            </div>
+          </div>
+          <Image
+            src="/images/main_info_second.jpeg"
+            width={880}
+            height={660}
+            alt="main_image_second"
+            className={clsx(
+              styles["info-section-cards-card-image"],
+              styles["info-section-cards-card-reverse-image"]
+            )}
+          />
+        </div>
+        <div className={clsx(styles["info-section-cards-card"])}>
+          <div className={styles["info-section-cards-card-text"]}>
+            <span className={styles["info-section-cards-card-text-title"]}>
+              Leading{" "}
+              <span
+                className={styles["info-section-cards-card-text-title-special"]}
+              >
+                Facilities
+              </span>
+            </span>
+            <div className={styles["info-section-cards-card-text-body"]}>
+              Over the years SNS has invested in a 24/7 learning and living
+              community that includes an expansive new academic building and
+              renovations to dorms, the library, dining hall, and athletic
+              facilities.
+            </div>
+          </div>
+          <Image
+            src="/images/main_info_third.jpeg"
+            width={880}
+            height={660}
+            alt="main_image_third"
+            className={clsx(styles["info-section-cards-card-image"])}
+          />
+        </div>
+        <div
+          className={clsx(
+            styles["info-section-cards-card"],
+            styles["info-section-cards-card-reverse"]
+          )}
+        >
+          <div className={styles["info-section-cards-card-text"]}>
+            <span className={styles["info-section-cards-card-text-title"]}>
+              Leading{" "}
+              <span
+                className={styles["info-section-cards-card-text-title-special"]}
+              >
+                Opportunities
+              </span>
+            </span>
+            <div className={styles["info-section-cards-card-text-body"]}>
+              SNS leads the way in providing opportunities that allow for the
+              discovery of new interests or a deeper immersion into long-held
+              passions. Faculty, coaches, dorm parents, deans, and advisors work
+              closely with students to guide their experiences.
+            </div>
+          </div>
+          <Image
+            src="/images/main_info_fourth.jpeg"
+            width={880}
+            height={660}
+            alt="main_image_fourth"
+            className={clsx(
+              styles["info-section-cards-card-image"],
+              styles["info-section-cards-card-reverse-image"]
+            )}
           />
         </div>
       </div>
