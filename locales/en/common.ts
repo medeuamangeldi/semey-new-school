8e23f53411ca3ddb2aa9189339bf8e452afa930a
Medeu Amangeldi <medcod49@gmail.com>
2024-04-24 21:54:46 -0500

    feat/translations, mission

--- locales/en/common.ts
+++ locales/en/common.ts
@@ -24,4 +24,38 @@ export const commonEn: any = {
   cardFourTitleTwo: "Opportunities",
   cardFourText:
     "SNS leads the way in providing opportunities that allow for the discovery of new interests or a deeper immersion into long-held passions. Faculty, coaches, dorm parents, deans, and advisors work closely with students to guide their experiences.",
+  ourMissionTitleOne: "Our",
+  ourMissionTitleTwo: "Mission",
+  ourMissionOneTitle:
+    "Fostering Potential: Empowering Minds for a Changing World",
+  ourMissionOneText:
+    "Our school is committed to fostering a dynamic learning environment where every student is empowered to reach their fullest potential, cultivating critical thinking, creativity, and compassion to thrive in a rapidly changing world.",
+  ourMissionTwoTitle: "Our Values",
+  ourMissionTwoTextFirst: "Excellence",
+  ourMissionTwoTextFirstDesc:
+    "Striving for the highest standards of achievement in academics, character, and personal growth.",
+  ourMissionTwoTextSecond: "Inclusivity",
+  ourMissionTwoTextSecondDesc:
+    "Embracing diversity and creating a welcoming environment where every individual feels valued and respected.",
+  ourMissionTwoTextThird: "Integrity",
+  ourMissionTwoTextThirdDesc:
+    "Upholding honesty, ethics, and moral principles in all actions and decisions.",
+  ourMissionTwoTextFourth: "Collaboration",
+  ourMissionTwoTextFourthDesc:
+    "Encouraging teamwork and cooperation among students, staff, parents, and the wider community to achieve common goals.",
+  ourMissionTwoTextFifth: "Innovation",
+  ourMissionTwoTextFifthDesc:
+    "Embracing creativity, curiosity, and adaptability to foster a culture of continuous improvement and exploration.",
+  ourMissionTwoTextSixth: "Resilience",
+  ourMissionTwoTextSixthDesc:
+    "Cultivating perseverance, grit, and a positive mindset to overcome challenges and bounce back from setbacks.",
+  ourMissionTwoTextSeventh: "Service",
+  ourMissionTwoTextSeventhDesc:
+    "Promoting a spirit of giving back and making a positive impact on the local and global community through acts of kindness and service.",
+  ourMissionTwoTextEighth: "Empowerment",
+  ourMissionTwoTextEighthDesc:
+    "Providing opportunities for students to take ownership of their learning, make informed choices, and become leaders in their own right.",
+  ourMissionThreeTitle: "Strategic Goals",
+  ourMissionThreeText:
+    "To implement personalized learning initiatives that cater to diverse student needs, leveraging technology and innovative teaching methodologies to enhance student engagement, academic achievement, and overall well-being.",
 };
