18908a9e53132bcf58a96b34a8bc008be821839c
Medeu Amangeldi <medcod49@gmail.com>
2024-04-24 22:04:19 -0500

    feat/translations, mission

--- src/app/_components/MainInfo/main-info.component.tsx
+++ src/app/_components/MainInfo/main-info.component.tsx
@@ -45,8 +45,8 @@ const MainInfo = () => {
           </div>
           <Image
             src="/images/main_info_first.jpeg"
-            width={880}
-            height={660}
+            width={700}
+            height={600}
             alt="main_image_first"
             className={styles["info-section-cards-card-image"]}
           />
@@ -75,8 +75,8 @@ const MainInfo = () => {
           </div>
           <Image
             src="/images/main_info_second.jpeg"
-            width={880}
-            height={660}
+            width={700}
+            height={600}
             alt="main_image_second"
             className={clsx(
               styles["info-section-cards-card-image"],
@@ -103,8 +103,8 @@ const MainInfo = () => {
           </div>
           <Image
             src="/images/main_info_third.jpeg"
-            width={880}
-            height={660}
+            width={700}
+            height={600}
             alt="main_image_third"
             className={clsx(styles["info-section-cards-card-image"])}
           />
@@ -133,8 +133,8 @@ const MainInfo = () => {
           </div>
           <Image
             src="/images/main_info_fourth.jpeg"
-            width={880}
-            height={660}
+            width={700}
+            height={600}
             alt="main_image_fourth"
             className={clsx(
               styles["info-section-cards-card-image"],
